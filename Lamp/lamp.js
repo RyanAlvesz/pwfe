0f4663d05e457a5579eab48c70cdc3c45f674dea
Ryan Alves <ra546038@gmail.com>
2023-08-09 14:30:24 -0300

    Atividade - Lmâmapada finalizada

--- Lamp/lamp.js
+++ Lamp/lamp.js
@@ -1,11 +1,21 @@
 'use strict'
 
-const ligar = document.getElementById('ligar')
-const desligar = document.getElementById('desligar')
+const botaoLigar = document.getElementById('ligar')
+const botaoDesligar = document.getElementById('desligar')
 const lamp = document.getElementById('lamp')
 
 function ligar () {
-    lamp.src = 'ligada.jpg'
+    lamp.src = './img/ligada.jpg'
 }
 
-ligar.addEventListener('click', ligar)
+function desligar () {
+    lamp.src = './img/desligada.jpg'
+}
+
+function quebrar () {
+    lamp.src = './img/quebrada.jpg'
+}
+
+botaoLigar.addEventListener('click', ligar)
+botaoDesligar.addEventListener('click', desligar)
+lamp.addEventListener('dblclick', quebrar)
